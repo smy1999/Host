26bee5fab599bf21b577a00ee4cf351b0d3f12fb
smy1999 <865988027@qq.com>
2020-10-14 18:41:53 +0800

    First modify .py

---
+++ firstPy.py
@@ -0,0 +1,32 @@
+# 001.注释使用
+print("hello world!")  # 井号前必须两个空格
+# good
+print()  # 默认带换行符
+print("nice Python")
+"""
+This is multi line.
+This is multi line.
+"""
+
+# 002.四则运算(直接除, 取整除, 字符串乘法)
+# 10 / 20 = 0.5
+# 9 // 2 = 4
+"=" * 10  # 字符串可以用乘法
+print("?" * 19)
+
+# 003.变量类型
+price = 12.3
+weight = 999
+money = price * weight
+print(weight)
+print(price)
+print(money)
+# int float bool str 不指定类型
+# Python3不区分long和int
+print(type(money))  # type()显示变量类型
+
+# 004.变量计算
+str1 = "hello "
+str2 = "world !"
+str3 = str1 + str2
+print(str3)

43f859749707686e2f7b6ee776504d3742dd875e
smy1999 <865988027@qq.com>
2020-10-15 11:06:20 +0800

    update PurchaseApple.py

---
+++ PurchaseApple.py
@@ -0,0 +1,7 @@
+priceStr = input("请输入单价 : ")
+weightStr = input("请输入重量 :")
+price = float(priceStr)
+weight = float(weightStr)
+print("价格为" + str(price * weight))
+
+print("价格为" + str(float(input("请输入单价 : ")) * float(input("请输入重量"))))
--- firstPy.py
+++ firstPy.py
@@ -24,10 +24,35 @@ print(money)
 # Python3不区分long和int
 print(type(money))  # type()显示变量类型
 
-# 004.变量计算
+# 004.字符计算
 str1 = "hello "
 str2 = "world !"
 str3 = str1 + str2
 print(str3)
+print((str1 + str2) * 10)
+print(str(price) + str1)  # str()将int转换为string
+print("10" + str1)
 
-# test cloud
+# 005.bool型视为0和1,大于0视为True
+what = True
+x1 = 10
+print(what + x1)
+
+# 006.input输入为string
+inputData = input("Please Input : ")
+print(inputData)
+
+# 007.类型转换函数
+str4 = "12"
+str5 = "12.4"
+int(str4)
+float(str5)
+
+# 008.
+
+
+
+# 009.
+# 010.
+# 011.
+# 012.
